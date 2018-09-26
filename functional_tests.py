198e0b69417e173cbc3c0c4ac9723ff9f98459b5
Jerry Kuo <jcdkuo@gmail.com>
2018-09-26 19:01:46 +0800

    首次FT，用註解說明規格，這次使用 unittest

--- functional_tests.py
+++ functional_tests.py
@@ -1,7 +1,45 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
 
-assert 'Django' in browser.title
+class NewVisitorTest(unittest.TestCase):
 
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retriee_it_later(self):
+        # Edith 聽到一個很酷的新線上待辦事項 app
+        # 她去查看它的首頁
+        self.browser.get('http://localhost:8000')
+
+        # 她發現網頁標題與標頭顯示待辦事項清單
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test!')
+
+        # 她馬上受邀輸入一個待辦事項
+
+# 她在文字方塊輸入“購買孔雀羽毛“
+# (Edith 的興趣是綁蒼蠅魚餌)
+
+# 當她按下 enter 時，網頁會更新，現在網頁列出
+# “1：購買孔雀羽毛“，一個待辦事項清單項目
+# 此時仍然有一個文字方塊，讓她可以加入另一個項目。
+# 她輸入“使用孔雀羽毛來製作一隻蒼蠅“（Edith 非常有條理）
+
+# 網頁再次更新，現在她的清單有這兩個項目
+
+# Edith 不知道網站能否記得她的清單
+# 接著她看到網站產生一個唯一的 URL 給她
+# 網頁有一些文字說明這個效果
+
+# 她前往那個 URL - 她的待辦清單仍然在那裡。
+
+# 她很滿意地上床睡覺
+
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
